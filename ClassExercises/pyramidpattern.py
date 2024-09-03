5377cb1c808445ab8590b745db171392c670932d
eobabs <eobabs@gmail.com>
2024-09-03 12:58:07 +0100

    Tuesday 03-09-2024

---
+++ ClassExercises/pyramidpattern.py
@@ -0,0 +1,8 @@
+pyramid_number = int(input("Enter a number: "))
+
+for number in range (1, pyramid_number+1):
+	print()
+	for counter in range (number, 0):
+		
+		space = " "
+		print (space * (pyramid_number - counter), number, end = " ")
---
+++ ClassExercises/smallpattern.py
@@ -0,0 +1,4 @@
+for counter in range(5, 0, -1) :
+	print ()
+	for count in range (counter, 0, -1):
+		print (count, end = ' ')
