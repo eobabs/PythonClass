bb37b842d74ebc2699d8e6215d653c1e48880223
eobabs <eobabs@gmail.com>
2024-10-11 14:45:05 +0100

    FizzBuzz

--- phaseGateFive/returnoninvestment.py
+++ phaseGateFive/returnoninvestment.py
@@ -2,12 +2,12 @@ INTEREST_RATE = 0.1
 
 print("Year   Interest      Amount")
 
-def get_ROI(number, time):
+def get_ROI(amount, time):
 
 	for year in range(1, time + 1):
-		interest = INTEREST_RATE * number
-		new_amount = interest + number
-		number = new_amount
+		interest = INTEREST_RATE * amount
+		new_amount = interest + amount
+		amount = new_amount
 		
 		print(f"{year}      {interest:,.2f}     {new_amount:,.2f}")
 
