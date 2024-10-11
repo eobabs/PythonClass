5a957acd38a57815726e1177c114ce601ccb53f3
eobabs <eobabs@gmail.com>
2024-10-11 07:04:27 +0100

    Fibonacci Rechecked

--- phaseGateFour/fibonacci.py
+++ phaseGateFour/fibonacci.py
@@ -1,15 +1,12 @@
 number = int(input("Enter your number to break at: "))
 
-
-
 number_one = 0
 number_two = 1
-temp_number = number_two
-count = 0
+temp_number = 1
 
 print (number_one, number_two, number_two, end =" ")
 
-for check in range (number + 1):
+for check in range (number - 3):
 	number_one = number_two
 	number_two = temp_number
 	temp_number = number_one + number_two
