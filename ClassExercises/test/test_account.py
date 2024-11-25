d1c5aedf566fcd59ec4ae25f9e356aaef0b1bc33
eobabs <eobabs@gmail.com>
2024-11-25 17:17:21 +0100

    Newest Tasks

--- ClassExercises/test/test_account.py
+++ ClassExercises/test/test_account.py
@@ -22,15 +22,38 @@ class TestAccount (unittest.TestCase):
 
     def test_withdrawal_function(self):
         self.account_one.deposit(Decimal('1000'))
-        self.account_one.withdraw(Decimal('100'))
+        self.account_one.change_pin("2000")
+        self.account_one.withdraw(Decimal('100'), "2000")
         self.assertEqual(self.account_one.get_balance(), Decimal('900'))
+
+    def test_withdrawal_function_requires_correct_pin(self):
+        self.account_one.deposit(Decimal('1000'))
+        self.account_one.change_pin("2000")
+        self.assertRaises(ValueError,self.account_one.withdraw, Decimal('100'), "9000")
+        # self.assertEqual(self.account_one.get_balance(), Decimal('900'))
 
     def test_withdrawal_function_does_not_allow_negatives(self):
         self.account_one.deposit(Decimal('1000'))
+        self.account_one.change_pin("2000")
         # account_one.withdraw(Decimal('-100'))
-        self.assertRaises(ValueError,self.account_one.withdraw, Decimal('-100'))
+        self.assertRaises(ValueError,self.account_one.withdraw, Decimal('-100'), "2000")
 
     def test_that_withdrawal_function_checks_insufficient_balance(self):
         self.account_one.deposit(Decimal('1000'))
+        self.account_one.change_pin("2000")
         # print(account_one.balance)
-        self.assertRaises(ValueError,self.account_one.withdraw, Decimal('10000'))
+        self.assertRaises(ValueError,self.account_one.withdraw, Decimal('10000'), "2000")
+
+    def test_that_pin_is_changed(self):
+        self.account_one.deposit(Decimal('1000'))
+        self.account_one.change_pin("2000")
+        self.assertEqual(self.account_one.get_balance(), Decimal('1000'))
+        self.assertEqual(self.account_one.pin,"2000")
+        self.account_one.withdraw(Decimal('100'), "2000")
+        self.assertEqual(self.account_one.get_balance(), Decimal('900'))
+        self.assertRaises(ValueError, self.account_one.withdraw, Decimal('100'), "0000")
+        self.account_one.change_pin("2060")
+        self.assertRaises(ValueError, self.account_one.withdraw, Decimal('100'), "2000")
+
+
+
